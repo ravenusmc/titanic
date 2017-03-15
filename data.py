a56b372732bd44f3377b970cab34c12a99793bb1
ravenusmc <mcuddy77@gmail.com>
2017-03-15 16:12:52 -0400

    added a data file and as well as a new page

---
+++ data.py
@@ -0,0 +1,10 @@
+#This file will hold the class that will manipulate the data on the titanic.
+
+
+#Importing files that will be used for the project
+import pandas as pd
+import numpy as np
+
+class Data():
+
+    #The following method 
--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from flask import Flask, render_template, request
 #Setting up flask
 app = Flask(__name__)
 
+#This code will send the user to the index/home page
 @app.route('/')
 def index():
     return render_template('index.html', title='Home Page')
+
+#This code will allow the user to go to a page to look at data.
+@app.route('/data')
+def data():
+    return render_template('data.html', title='Data Page')
 
 #This line will actually run the app.
 app.run(debug=True)
